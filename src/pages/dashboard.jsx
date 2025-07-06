f412e8c867b4eb7aa07d0ab2c8e83ee23a5d0fe0
ICKY202 <vigneshm674@gmail.com>
2025-07-06 13:39:06 +0530

    implemented to urls filter functionality and made API calls for clicks and urls to populated in dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,19 +1,41 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FilterIcon } from "lucide-react";
 import { BarLoader } from "react-spinners";
+import {useEffect, useState} from 'react';
+
+import {getUrls} from '../db/apiUrls';
+import {getClicks} from '../db/apiClicks';
+import useFetch from "@/hooks/use-fetch";
+import { UrlState } from "@/context";
 
 const Dashboard = () => {
+    const [query, setQuery] = useState("");
+    const {data: user} = UrlState();
+    const {data: urls, error, loading, fetchData: fnUrls} = useFetch(getUrls, user?.id);
+    const {data: clicks, loading:loadingClicks, fetchData: fnClicks} = useFetch(getClicks, urls?.map((url) => url.id));
 
-    const loader = false;
-    return <div className="flex flex-col gep-2">
-            {loader && <BarLoader width={"100%"} color="#397f75"/>}
+    useEffect(() => {
+        fnUrls();
+    }, [urls?.length]);
+
+    useEffect(() => {
+        if(urls.length) fnClicks();
+    }, [urls?.length]);
+
+    const filteredUrls = urls?.filter((url) => url.title.toLowserCase().includes(query.toLowerCase()));
+
+
+    return <div className="flex flex-col gap-4">
+            {loading || loadingClicks && <BarLoader width={"100%"} color="#397f75"/>}
             <div className="grid grid-cols-2 gap-4">
                 <Card>
                     <CardHeader>
                         <CardTitle>Links created</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        <p>0</p>
+                        <p>{urls?.length}</p>
                     </CardContent>
                 </Card>
                 <Card>
@@ -21,7 +43,7 @@ const Dashboard = () => {
                         <CardTitle>Link Clicks</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        <p>0</p>
+                        <p>{clicks.length}</p>
                     </CardContent>
                 </Card>
             </div>
@@ -29,7 +51,14 @@ const Dashboard = () => {
                 <h1 className="text-4xl font-extrabold">My Links</h1>
                 <Button>Create Link</Button>
             </div>
-            
+            <div className="relative">
+                <Input value={query} type="text" placeholder="Filter the urls" onChange={(e) => setQuery(e.target.value)}/>
+                <FilterIcon className="absolute top-2 right-1 p-1"/>
+            </div>
+            {error && <Error message={error?.message} />}
+            {(filteredUrls || []).map((url) => {
+                return url.title;
+            })}
     </div>
 
 }
